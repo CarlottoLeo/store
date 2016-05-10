a58208ee2bc890eec1e24435f05f56bbda4b2d60
Leonardo Falk <leonardo.falk@hotmail.com>
2016-05-10 16:26:58 -0300

    Fixed some tests that required JS enabled

--- spec/views/products_spec.rb
+++ spec/views/products_spec.rb
@@ -1,8 +1,10 @@
 require "rails_helper"
 
 RSpec.feature Product, :type => :feature do
-  context "create a product" do
+context "create a product" do
     scenario "success", js: true do
+      Product.destroy_all
+
       visit "/products/new"
 
       fill_in "Name",    :with => "Produto Teste RSPEC"
@@ -10,10 +12,14 @@ RSpec.feature Product, :type => :feature do
 
       execute_script("$('#product-form').submit();")
 
+      sleep 2.second
+
       expect(page).to have_text("Product was successfully created.")
     end
 
     scenario "failure", js: true do
+      Product.destroy_all
+
       visit "/products/new"
 
       fill_in "Name",    :with => "Produto Teste RSPEC"
@@ -21,10 +27,14 @@ RSpec.feature Product, :type => :feature do
 
       execute_script("$('#product-form').submit();")
 
+      sleep 2.second
+
       expect(page).to have_text("Value must be greater than 0.01")
     end
 
     scenario "failure with higher floating precision", js: true do
+      Product.destroy_all
+
       visit "/products/new"
 
       fill_in "Name",    :with => "Produto Teste RSPEC"
@@ -32,87 +42,96 @@ RSpec.feature Product, :type => :feature do
 
       execute_script("$('#product-form').submit();")
 
+      sleep 2.second
+
       expect(page).to have_text("Value must be greater than 0.01")
     end
   end
 
-  context "edit a product" do
-    scenario "success" do
+  context "update a product" do
+    scenario "success", :js => true do
+      Product.destroy_all
+
       prod = Product.create({name: "Produto Teste RSPEC", value: "10.0"})
 
-      visit '/products/1/edit'
+      visit edit_product_path(prod)
 
-      fill_in "Name",    :with => "Nome editado"
-      fill_in "Value",   :with => "5.0"
+      fill_in "Name",    :with => "Produto Teste RSPEC"
+      fill_in "Value",   :with => "100,0"
 
       execute_script("$('#product-form').submit();")
+
+      sleep 2.second
 
       expect(page).to have_text("Product was successfully updated.")
     end
 
-    scenario "failure" do
+    scenario "failure", :js => true do
+      Product.destroy_all
+
       prod = Product.create({name: "Produto Teste RSPEC", value: "10.0"})
 
-      visit '/products/1/edit'
+      visit edit_product_path(prod)
 
       fill_in "Name",    :with => ""
       fill_in "Value",   :with => ""
 
       execute_script("$('#product-form').submit();")
 
+      sleep 2.second
+
       expect(page).to have_text("Oops")
     end
 
-    scenario "failure with higher floating precision" do
+    scenario "failure with higher floating precision", :js => true do
+      Product.destroy_all
+
       prod = Product.create({name: "Produto Teste RSPEC", value: "10.0"})
 
-      visit '/products/1/edit'
+      visit edit_product_path(prod)
 
       fill_in "Name",    :with => "Produto Teste RSPEC"
-      fill_in "Value",   :with => "0.0000001"
+      fill_in "Value",   :with => "0,0000001"
 
       execute_script("$('#product-form').submit();")
+
+      sleep 2.second
 
       expect(page).to have_text("Oops")
     end
 
-    scenario "failure with empty name" do
+    scenario "failure with empty name", :js => true do
+      Product.destroy_all
+
       prod = Product.create({name: "Produto Teste RSPEC", value: "10.0"})
 
-      visit '/products/1/edit'
+      visit edit_product_path(prod)
 
       fill_in "Name",    :with => ""
-      fill_in "Value",   :with => "0.0000001"
+      fill_in "Value",   :with => "0,0000001"
 
       execute_script("$('#product-form').submit();")
+
+      sleep 2.second
 
       expect(page).to have_text("Name can't be blank")
     end
 
-    scenario "failure with empty value" do
+    scenario "failure with empty value", :js => true do
+      Product.destroy_all
+
       prod = Product.create({name: "Produto Teste RSPEC", value: "10.0"})
 
-      visit '/products/1/edit'
+      visit edit_product_path(prod)
 
       fill_in "Name",    :with => "Produto Teste RSPEC"
       fill_in "Value",   :with => ""
 
       execute_script("$('#product-form').submit();")
 
+      sleep 2.second
+
       expect(page).to have_text("Value is not a number")
     end
   end
-
-#  [[not working]]
-#  context "destroy a product" do
-#    scenario "success" do
-#      Product.create({name: "Produto Teste RSPEC", value: "10.0"})
-#
-#      visit "/products"
-#
-#      send "/products/1", :method => :delete
-#      #expect { click_link 'Destroy' }.to change(Category, :count).by(-1)
-#      expect(Product.find_by_id(1)).to eq(nil)
-#    end
-#  end
 end
