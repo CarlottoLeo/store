85e7bf214762277df8d055c7157b8d5da6248420
Leonardo Falk <leonardo.falk@hotmail.com>
2016-05-30 16:04:10 -0300

    Fixed name validation regex

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,22 @@
 class Product < ActiveRecord::Base
   validates_presence_of :name
-  validates_presence_of :value
+  validates_length_of   :name, minimum: 3, maximum: 30
+  validates_format_of   :name, with: /[\w]+([\s]?+[\w]+){1}+/
 
+  validates_presence_of :value
   validates_numericality_of :value, greater_than: 0.01, allow_nil: false
 
   scope :get_all_by_name_asc, -> {order(name: :asc)}
 
+  before_save :capitalize_name
+
   audited
 
   acts_as_paranoid
+
+  def capitalize_name
+    self.name = self.name.titleize
+  end
 
   def self.get_collection_for_select_tag
     prods = Product.get_all_by_name_asc
